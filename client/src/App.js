8de3f18dd5d8dd474c9712992a904d6e96ed81d4
Kaustubh Pathak <kaustubhpathak@gmail.com>
2023-08-26 18:27:44 +0530

    Finally ! debugged , working properly ...

---
+++ client/src/App.js
@@ -0,0 +1,46 @@
+import "./App.css";
+import { useState } from "react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+import CssBaseline from "@mui/material/CssBaseline";
+
+import ResponsiveAppBar from "./Components/Navbar/Navbar"
+
+import RouteConfig from "./RouteConfig.jsx";
+
+
+const lightTheme = createTheme({
+  palette: {
+    mode: "light",
+  },
+});
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+function App() {
+  const [checked, setChecked] = useState(false);
+  const handleTheme = () => {
+    setChecked(!checked);
+    localStorage.setItem("theme", checked);
+  };
+  var isTrueSet = localStorage.getItem("theme") === "true";
+  if (isTrueSet) {
+    var theme = darkTheme;
+  } else {
+    theme = lightTheme;
+  }
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className="App">
+        <ResponsiveAppBar change={handleTheme} />
+        <RouteConfig />
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ client/src/RouteConfig.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+
+
+
+import LandingPage from './Pages/Landing Page/LandingPage';
+import UserLogin from './Pages/User/UserLogin/UserLogin';
+import UserDash from './Pages/User/UserDash/UserDash';
+
+
+
+const RouteConfig = () => {
+  return (
+        <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<LandingPage />}></Route>
+                    <Route path='/user/login' element={<UserLogin />} />
+                    <Route path='/user/dash' element={<UserDash />}/>
+                </Routes>
+
+        </BrowserRouter>
+    )
+}
+
+export default RouteConfig
