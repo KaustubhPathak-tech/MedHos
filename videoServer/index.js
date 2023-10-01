4abdd050351195639667fa001000a958ffef1e4f
Kaustubh Pathak <kaustubhpathak@gmail.com>
2023-10-01 19:54:36 +0530

    ww

--- videoServer/index.js
+++ videoServer/index.js
@@ -1,44 +1,43 @@
 import express from "express";
-import dotenv from "dotenv";
-import { Server } from "socket.io";
-const app=express();
-dotenv.config();
 
-const io = new Server(process.env.ioPORT||8001, { cors: true });
+import { Server } from "socket.io";
+const app = express();
+
+const io = new Server(8001, { cors: true });
 
 const etoSockets = new Map();
 const socketToEts = new Map();
 io.listen(8003);
-app.get("/",(req,res)=>{res.send("hello")});
-app.listen(process.env.PORT||8002,()=>console.log("server started"));
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+app.listen(8002, () => console.log("server started"));
 io.on("connection", (socket) => {
-    console.log("socket connected", socket.id);
-    socket.on("room:join", ({ Email, Room }) => {
-      etoSockets.set(Email, socket.id);
-      socketToEts.set(socket.id, Email);
-      io.to(Room).emit("user:joined", { Email, id: socket.id });
-      socket.join(Room);
-      io.to(socket.id).emit("room:join", { Email, Room });
-    });
-    socket.on("disconnect", () => {
-      
-      console.log("socket disconnected");
-    });
-  
-    socket.on("user:call", ({ to, offer }) => {
-      io.to(to).emit("incoming:call", { from:socket.id,offer });
-    });
-    socket.on("call:accepted", ({ to, ans }) => {
-      io.to(to).emit("call:accepted", { from:socket.id,ans });
-    });
-  
-    socket.on("peer:nego:needed", ({ to, offer }) => {
-      console.log("peer:nego:needed",offer);
-      io.to(to).emit("peer:nego:needed", { from:socket.id,offer });
-    });
-    socket.on("peer:nego:done", ({ to, ans }) => {
-      console.log("peer:nego:done",ans);
-      io.to(to).emit("peer:nego:final", { from:socket.id,ans });
-    });
-  
+  console.log("socket connected", socket.id);
+  socket.on("room:join", ({ Email, Room }) => {
+    etoSockets.set(Email, socket.id);
+    socketToEts.set(socket.id, Email);
+    io.to(Room).emit("user:joined", { Email, id: socket.id });
+    socket.join(Room);
+    io.to(socket.id).emit("room:join", { Email, Room });
   });
+  socket.on("disconnect", () => {
+    console.log("socket disconnected");
+  });
+
+  socket.on("user:call", ({ to, offer }) => {
+    io.to(to).emit("incoming:call", { from: socket.id, offer });
+  });
+  socket.on("call:accepted", ({ to, ans }) => {
+    io.to(to).emit("call:accepted", { from: socket.id, ans });
+  });
+
+  socket.on("peer:nego:needed", ({ to, offer }) => {
+    console.log("peer:nego:needed", offer);
+    io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
+  });
+  socket.on("peer:nego:done", ({ to, ans }) => {
+    console.log("peer:nego:done", ans);
+    io.to(to).emit("peer:nego:final", { from: socket.id, ans });
+  });
+});
