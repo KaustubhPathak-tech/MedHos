3b38258201e0b9cc06de2a62815682b60b7353f7
Kaustubh Pathak <kaustubhpathak@gmail.com>
2023-10-01 18:18:18 +0530

    updating the file

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-const API = axios.create({ baseURL: "https://medhosserver.onrender.com" });// https://medhosserver.onrender.com/ https://fine-puce-hen-wig.cyclic.cloud
+const API = axios.create({ baseURL: "https://fine-puce-hen-wig.cyclic.cloud" });// https://medhosserver.onrender.com/ https://fine-puce-hen-wig.cyclic.cloud
 
 export const logIn = (authData) => API.post("/user/login", authData);
 export const dlogIn = (authData) => API.post("/doctor/login", authData);
