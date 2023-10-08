129d8c5f317389a80d4912426e6f6824544b9e6f
Kaustubh Pathak <kaustubhpathak@gmail.com>
2023-10-08 15:32:44 +0530

    slow and nicer!

--- server/controllers/order.js
+++ server/controllers/order.js
@@ -25,17 +25,26 @@ export const saveOrder = async (req, res) => {
 };
 export const verifyPayment = async (req, res) => {
   const { userId } = req.body;
+  // console.log(typeof(userId));
 
   try {
-    const user1 = await users.findById(userId);
-    const order = await Order.find({ user: userId });
-    if (order[order.length - 1].status === "Order Confirmed") {
-      res.status(201).send({
+    const user1 = await users.findOne({ _id: userId });
+    // const order = await Order.find({ user: userId });
+    // console.log(order[order.length - 1].status);
+    // if (order[order.length - 1].status === "Order Confirmed") {
+    //   res.status(201).send({
+    //     success: true,
+    //     message: "Order already confirmed",
+    //     data: user1.cart,
+    //   });
+    // }
+    res
+      .status(201)
+      .send({
         success: true,
         message: "Order already confirmed",
         data: user1.cart,
       });
-    }
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
