07b3d15935d922635f250a2f75364b6d85765b50
Kaustubh Pathak <kaustubhpathak@gmail.com>
2023-10-08 18:34:31 +0530

    big fix

--- paymentServer/index.js
+++ paymentServer/index.js
@@ -1,4 +1,4 @@
-import express, { response } from "express";
+import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 
@@ -25,88 +25,81 @@ app.post(
   "/webhook",
   express.raw({ type: "application/json" }),
   async (request, response) => {
-    let event = request.body;
+    let secret = endpointSecret;
+    const sig = request.headers["stripe-signature"];
+    const payload = request.body;
 
-    if (endpointSecret) {
-      // Get the signature sent by Stripe
-      const signature = request.headers["stripe-signature"];
-      try {
-        event = stripeInstance.webhooks.constructEvent(
-          request.body,
-          signature,
-          endpointSecret
-        );
-      } catch (err) {
-        console.log(`⚠️  Webhook signature verification failed.`, err.message);
-        return response.sendStatus(400);
+    let event;
+
+    try {
+      event = stripeInstance.webhooks.constructEvent(payload, sig, secret);
+      console.log(event);
+      switch (event.type) {
+        case "payment_intent.succeeded":
+          console.log("PaymentIntent was successful!");
+          const paymentIntentData = event.data.object.payment_intent_data;
+          const userId = paymentIntentData.metadata;
+
+          // Use the userId to update userInfo or perform other actions.
+          console.log("Completed for user ID: ", userId);
+          break;
+        case "payment_intent.payment_failed":
+          console.log("PaymentIntent failed!");
+          break;
+        case "customer.created":
+          console.log("customer created");
+          break;
+        case "charge.succeeded":
+          const successs = event.data.object;
+          const Mydata = successs.metadata;
+          try {
+            const user = await users.findOne({ _id: Mydata.userId });
+            console.log(user);
+            user.cart = [];
+            await user.save();
+            // Then define and call a function to handle the event checkout.session.completed
+            const order = await Order.findOne({ orderId: Mydata.orderId });
+            order.status = "Order Confirmed";
+            order.confirmed = true;
+            await order.save();
+          } catch (error) {
+            console.log(error.message);
+          }
+          console.log("charge succeeded", Mydata);
+          break;
+        case "checkout.session.async_payment_failed":
+          const checkoutSessionAsyncPaymentFailed = event.data.object;
+          // Then define and call a function to handle the event checkout.session.async_payment_failed
+          console.log("Failed");
+          break;
+        case "checkout.session.async_payment_succeeded":
+          const checkoutSessionAsyncPaymentSucceeded = event.data.object;
+
+          console.log("Success");
+          break;
+        case "checkout.session.completed":
+          const checkoutSessionCompleted = event.data.object;
+          // const metadata = checkoutSessionCompleted.metadata;
+          // console.log("Completed", metadata);
+          //
+          break;
+        case "checkout.session.expired":
+          const checkoutSessionExpired = event.data.object;
+          // Then define and call a function to handle the event checkout.session.expired
+          console.log("Expired");
+          break;
+        // ... handle other event types
+        default:
+          console.log(`Unhandled event type ${event.type}`);
       }
+
+      response.status(200).json({ success: true }).end();
+    } catch (err) {
+      console.log(err.message);
+      response.status(404).json({ "Webhook Error": err.message });
+      return;
     }
-
-
-    switch (event.type) {
-      case "payment_intent.succeeded":
-        console.log("PaymentIntent was successful!");
-        const paymentIntentData = event.data.object.payment_intent_data;
-        const userId = paymentIntentData.metadata;
-
-        // Use the userId to update userInfo or perform other actions.
-        console.log("Completed for user ID: ", userId);
-        break;
-      case "payment_intent.payment_failed":
-        console.log("PaymentIntent failed!");
-        break;
-      case "customer.created":
-        console.log("customer created");
-        break;
-      case "charge.succeeded":
-        const successs = event.data.object;
-        const Mydata = successs.metadata;
-        try {
-          const user = await users.findOne({ _id: Mydata.userId });
-          console.log(user);
-          user.cart = [];
-          await user.save();
-          // Then define and call a function to handle the event checkout.session.completed
-          const order = await Order.findOne({ orderId: Mydata.orderId });
-          order.status = "Order Confirmed";
-          order.confirmed = true;
-          await order.save();
-        } catch (error) {
-          console.log(error.message);
-        }
-        console.log("charge succeeded", Mydata);
-        break;
-      case "checkout.session.async_payment_failed":
-        const checkoutSessionAsyncPaymentFailed = event.data.object;
-        // Then define and call a function to handle the event checkout.session.async_payment_failed
-        console.log("Failed");
-        break;
-      case "checkout.session.async_payment_succeeded":
-        const checkoutSessionAsyncPaymentSucceeded = event.data.object;
-
-        console.log("Success");
-        break;
-      case "checkout.session.completed":
-        const checkoutSessionCompleted = event.data.object;
-        // const metadata = checkoutSessionCompleted.metadata;
-        // console.log("Completed", metadata);
-        //
-        break;
-      case "checkout.session.expired":
-        const checkoutSessionExpired = event.data.object;
-        // Then define and call a function to handle the event checkout.session.expired
-        console.log("Expired");
-        break;
-      // ... handle other event types
-      default:
-        console.log(`Unhandled event type ${event.type}`);
-    }
-
-    response.send(200).json({ received: true }).end();
   }
-
-  // Return a response to acknowledge receipt of the event
-  
 );
 
 //stripe webhook end
