1ffb15b6aa4d10d2f021a4641204602d1e1fded0
Kaustubh Pathak <kaustubhpathak@gmail.com>
2023-11-25 14:07:43 +0530

    removing changes

--- client/src/Components/Context/SocketProvider.jsx
+++ client/src/Components/Context/SocketProvider.jsx
@@ -9,7 +9,7 @@ export const useSocket = () => {
 };
 
 export const SocketProvider = ({ children }) => {
-  const socket = useMemo(() => io("http://localhost:8001"), []); //https://otivaindustries.net   https://video-call-medhos.vercel.app
+  const socket = useMemo(() => io("https://otivaindustries.net"), []); //https://video-call-medhos.vercel.app http://localhost:8001
   return (
     <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
   );
